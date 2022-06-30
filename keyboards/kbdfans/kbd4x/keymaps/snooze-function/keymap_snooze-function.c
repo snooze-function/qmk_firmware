bca0dd977ec89afd703645ac65b96335965deb5a
snooze-function <39447780+snooze-function@users.noreply.github.com>
2022-06-30 21:55:52 +0200

    Add files via upload

--- keyboards/kbdfans/kbd4x/keymaps/snooze-function/keymap_snooze-function.c
+++ keyboards/kbdfans/kbd4x/keymaps/snooze-function/keymap_snooze-function.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 sevenseacat
+/* Copyright 2022 snooze-function
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,39 +29,64 @@ enum custom_keycodes {
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |MO(4) |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | GUI  | Alt  | M0(3)|Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+ 
 	[0] = LAYOUT_planck_mit(
-    KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-    KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(4),
-    KC_LCTL, KC_LGUI, KC_LALT, MO(3), MO(1), KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(4),
+    KC_LCTL, KC_LGUI, KC_LALT, MO(3),   MO(1),   KC_SPC,  /**/     MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  ),
+
 	[1] = LAYOUT_planck_mit(
-    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
-    KC_CAPS, ___, ___, ___, ___, ___, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, KC_SCLN, KC_PENT,
-    ___, ___, ___, ___, ___, ___, KC_MINS, KC_EQL, ___, ___, ___, ___,
-    ___, ___, ___, ___, ___, ___, ___, ___, KC_VOLD, KC_VOLU, KC_MUTE),
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_CAPS, ___,     ___,     ___,     ___,     ___,     KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, KC_SCLN, KC_PENT,
+    ___,     ___,     ___,     ___,     ___,     ___,     KC_MINS, KC_EQL,  ___,     ___,     ___,     ___,
+    ___,     ___,     ___,     ___,     ___,     ___,     /**/     ___,     ___,     KC_VOLD, KC_VOLU, KC_MUTE
+  ),
+
 	[2] = LAYOUT_planck_mit(
-    KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, ___,
-    ___, ___, ___, ___, ___, ___, KC_LCBR, KC_RCBR, KC_PIPE, KC_DQUO, KC_COLN, ___,
-    ___, ___, ___, ___, ___, ___, KC_UNDS, KC_PLUS, ___, ___, ___, ___,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, MO(5)),
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, ___,
+    ___,     ___,     ___,     ___,     ___,     ___,     KC_LCBR, KC_RCBR, KC_PIPE, KC_DQUO, KC_COLN, ___,
+    ___,     ___,     ___,     ___,     ___,     ___,     KC_UNDS, KC_PLUS, ___,     ___,     ___,     ___,
+    ___,     ___,     ___,     ___,     ___,     /**/      ___,     ___,     ___,     ___,     ___,     MO(5)
+  ),
+
 	[3] = LAYOUT_planck_mit(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-    KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,
-    ___, KC_PGDN, KC_PGUP, KC_END, KC_HOME, ___, ___, ___, ___, ___, ___, ___,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___),
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
+    KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,   KC_F24,
+    ___,     KC_PGDN, KC_PGUP, KC_END,  KC_HOME, ___,     ___,     ___,     ___,     ___,     ___,     ___,
+    ___,     ___,     ___,     ___,     ___,     ___,     /**/     ___,     ___,     ___,     ___,     ___
+  ),
+
 	[4] = LAYOUT_planck_mit(
-    KC_PAST, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, RALT(KC_MINS),
-    LCTL(KC_F1), LCTL(KC_F2), LCTL(KC_F3), LCTL(KC_F4), LCTL(KC_F5), LCTL(KC_F6), LCTL(KC_F7), LCTL(KC_F8), LCTL(KC_F9), LCTL(KC_F10), LCTL(KC_F11), LCTL(KC_F12),
-    ___, LCTL(KC_F9), LCA(KC_PSLS), RSA(KC_LBRC), RSA(KC_RBRC), LCTL(KC_PENT), KC_PMNS, KC_PPLS, RALT_T(KC_EQL), KC_EQL, RALT(KC_MINS), ___,
-    ___, ___, ___, ___, ___, RALT(KC_MINS), ___, RALT(KC_MINS), SGUI(KC_DOWN), SGUI(KC_UP), ___),
+    KC_PAST,     KC_P1,       KC_P2,        KC_P3,        KC_P4,        KC_P5,         KC_P6,       KC_P7,         KC_P8,          KC_P9,         KC_P0,         RALT(KC_MINS),
+    LCTL(KC_F1), LCTL(KC_F2), LCTL(KC_F3),  LCTL(KC_F4),  LCTL(KC_F5),  LCTL(KC_F6),   LCTL(KC_F7), LCTL(KC_F8),   LCTL(KC_F9),    LCTL(KC_F10),  LCTL(KC_F11),  LCTL(KC_F12),
+    ___,         LCTL(KC_F9), LCA(KC_PSLS), RSA(KC_LBRC), RSA(KC_RBRC), LCTL(KC_PENT), KC_PMNS,     KC_PPLS,       RALT_T(KC_EQL), KC_EQL,        RALT(KC_MINS), ___,
+    ___,         ___,         ___,          ___,          ___,          RALT(KC_MINS), /**/         ___,           RALT(KC_MINS),  SGUI(KC_DOWN), SGUI(KC_UP),   ___
+  ),
+
 	[5] = LAYOUT_planck_mit(
-    RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_NO, KC_NO, KC_NO,
-    RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, ___,
-    RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD, MO(6), ___, KC_NO, KC_NO, KC_NO, ___),
+    RGB_TOG, KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_NO,   KC_NO,   KC_NO,
+    RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,  RGB_MOD,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   ___,
+    RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,  RGB_RMOD, MO(6),   /**/     ___,     KC_NO,   KC_NO,   KC_NO,   ___
+  ),
+
 	[6] = LAYOUT_planck_mit(
-    QK_BOOT, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    ___, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, ___,
-    ___, ___, ___, ___, ___, ___, ___, KC_NO, KC_NO, KC_NO, ___)
+    QK_BOOT, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    ___,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   ___,
+    ___,     ___,     ___,     ___,     ___,     ___,     /**/     ___,     KC_NO,   KC_NO,   KC_NO,   ___
+  )
 };
